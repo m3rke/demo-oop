d9e1096f899b9caa9d2caa882998f68338a23f13
Анищенко Марк Эдуардович <gr401_anmed@ttit.local>
2022-03-23 12:15:59 +0700

    task22

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -6,17 +6,17 @@ const DIR_CONFIG = '/../config';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 //Добавляем пользовательскую функцию автозагрузки классов
-spl_autoload_register(function ($className) {
-    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-    $className = str_replace('\\', '/', $className);
-
-    foreach ($paths['classes'] as $path) {
-        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-        if (file_exists($fileName)) {
-            require_once $fileName;
-        }
-    }
-});
+//spl_autoload_register(function ($className) {
+//    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
+//    $className = str_replace('\\', '/', $className);
+//
+//    foreach ($paths['classes'] as $path) {
+//        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
+//        if (file_exists($fileName)) {
+//            require_once $fileName;
+//        }
+//    }
+//});
 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
@@ -32,6 +32,12 @@ function getConfigs(string $path = DIR_CONFIG): array
 }
 
 require_once __DIR__ . '/../routes/web.php';
+$app = new Src\Application(new Src\Settings(getConfigs()));
 
+//Функция возвращает глобальный экземпляр приложения
+function app() {
+    global $app;
+    return $app;
+}
 
-return new Src\Application(new Src\Settings(getConfigs()));
+return $app;
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,29 @@ class ComposerAutoloaderInit116305b54fac0c4abb97732d0e2e6be7
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit116305b54fac0c4abb97732d0e2e6be7::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire116305b54fac0c4abb97732d0e2e6be7($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire116305b54fac0c4abb97732d0e2e6be7($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
